31db17e50d683ff7c2292dfe38c4dfcac3371b1c
hypercube-ai-dev <nickwilliams@hypercubetrading.com>
2023-11-21 16:58:40 -0800

    naming for sub cube

--- simple_ne/sub_cube.py
+++ simple_ne/sub_cube.py
@@ -2,18 +2,18 @@ import torch
 import itertools
 
 class SubDivisionCube(object):
-    def __init__(self, dim, initial_depth, center, width):
+    def __init__(self, dim, depth, center, width):
         self.width = width
         self.center = center
         self.dim = dim
-        self.initial_depth = initial_depth
+        self.depth = depth
         self.signs = list(itertools.product([1,-1], repeat=len(dim)))
-        self.children = {}
+        self.div_array = {}
         self.initial_divide()
 
     def initial_divide(self):
         depth = 0
-        while depth < self.initial_depth:
+        while depth < self.depth:
             depth += 1
             num_children = (2**self.dim) ** depth
             offsets = torch.ones(num_children, self.dim) * (self.width / (2*depth))

09025749985df4de3046efa85967322ce980400e
hypercube-ai-dev <nickwilliams@hypercubetrading.com>
2024-03-13 21:35:57 -0700

    tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from simple_ne.simple_ne import SimpleNEAgent, SimpleNENode, SimpleNEPopulation
-
+from simple_ne.sub_cube import SubDivisionCube
+import torch
 
 def get_weights_tests():
     bp = SimpleNEPopulation(2, 2, 100, 10)
@@ -8,3 +9,10 @@ def get_weights_tests():
     print(test_genome.get_weights(True))
 
 get_weights_tests()
+
+def tree_test():
+    cube = SubDivisionCube((0.0,0.0,0.0), 2, .5)
+    print(cube.tree[0].shape)
+    print(len(cube.tree[0][1]))
+
+tree_test()

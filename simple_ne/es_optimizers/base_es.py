4d08ff1c9aa8b6d68f4e4d188a78cf44e743bf22
hypercube-ai-dev <nickwilliams@hypercubetrading.com>
2024-08-14 21:58:46 -0700

    finish up base class

--- simple_ne/es_optimizers/base_es.py
+++ simple_ne/es_optimizers/base_es.py
@@ -8,21 +8,13 @@ class BaseEsOptiizer():
         self.model = model
         self.pop_size = pop_size
         self.fit_func = fit_func
-    
-    def _evaluate(self, solutions, episodes):
-        rewards = []
-        for theta in solutions:
-            nn_utils.vector_to_parameters(theta, self.model.parameters())
-            reward = self._compute_fitness(episodes)
-            rewards.append(-reward)  # CMA-ES minimizes, so we use negative rewards
-        return rewards
 
     def _compute_fitness(self, params, episodes):
         nn_utils.vector_to_parameters(params, self.model.parameters())
         self.model.eval()
         total_reward = 0
         for _ in range(episodes):
-            reward = self.compute_fitness(self.model)
+            reward = self.fit_func(self.model)
             total_reward += reward
         return total_reward / episodes
     
